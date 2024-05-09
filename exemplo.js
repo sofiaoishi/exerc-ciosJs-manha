446bfb6f9f03c707f884609e40173764e71ce9a8
sofiaoishi <167095663+sofiaoishi@users.noreply.github.com>
2024-05-09 10:12:29 -0300

    Add files via upload

---
+++ exemplo.js
@@ -0,0 +1,36 @@
+//Apresentar alguma mensagem na tela
+// alert("Hello Word!")
+
+//Apresenta uma mensagem no console
+// console.log("Console:Hello Word!")
+
+//Apresenta uma mensagem
+// prompet( mensagem na tela que pega alguma informação )
+
+//Pegando um nome e apresentando na tela
+// let nome = prompt("Digite o seu nome: ")
+
+// console.log("Bem vindo! " + nome)
+
+
+//Fazendo conta com javascript
+
+//parseInt => Transforma o que está entre () em um número inteiro: 1, 2, 3. ....
+//let nota1 = parseInt(prompt("Digite uma nota"));
+
+//let nota2 = parseInt(prompt("Digite outra nota: "));
+
+
+// let media = (nota1 + nota2) /2
+
+// console.log(media)
+
+
+//parseFloat => Transforma o que está entre () em número com vírgula / número frasionado
+let pronduto1 = parseFloat(prompt("Digite o valor do produto: "))
+
+let produto2 = parseFloat(prompt("Digite o valor do segundo produto: "))
+
+let soma = pronduto1 + produto2;
+
+console.log(soma)
