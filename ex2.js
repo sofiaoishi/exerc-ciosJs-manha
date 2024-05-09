fabd753ffea4af2b50788e5147a52d92922397bb
sofiaoishi <167095663+sofiaoishi@users.noreply.github.com>
2024-05-09 10:32:39 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,38 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da 
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira, 
+// 2 representa terça-feira e assim por diante.
+
+// solicitar ao usuário um número de 1 a 7
+
+let numero = prompt("Digite um numero de 1 a 7: ")
+
+// definir dias da semana e exibir resposta ao usuario
+
+if (numero == 1) {
+    console.log("segunda-feira")
+}
+
+if (numero == 2) {
+    console.log("terca-feira")
+}
+
+if (numero == 3) {
+    console.log("quarta-feira")
+}
+
+if (numero == 4) {
+    console.log("quinta-feira")
+}
+
+if (numero == 5) {
+    console.log("sexta-feira")
+}
+
+if (numero == 6) {
+    console.log("sabado")
+}
+
+if (numero == 7) {
+    console.log("domingo")
+}
