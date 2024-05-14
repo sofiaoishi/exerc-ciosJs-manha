bbf91160c57a480d359dc455c2c456d60d6bc7a4
sofiaoishi <167095663+sofiaoishi@users.noreply.github.com>
2024-05-14 09:35:38 -0300

    Add files via upload

---
+++ exn4.js
@@ -0,0 +1,8 @@
+//FOR
+//4. Faça um programa que imprima na tela a tabuada de um número qualquer escolhido pelo usuário até o 10.
+
+let numero = 5
+
+for (let contador = 0; contador <= 10; contador ++) {
+        console.log(contador * numero);
+    }
