6280886e3e1c7cc2d511f5f384ff622838ec25cd
sofiaoishi <167095663+sofiaoishi@users.noreply.github.com>
2024-05-14 10:49:31 -0300

    Add files via upload

---
+++ exn5.js
@@ -0,0 +1,13 @@
+//FOR
+//5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+
+function fibonacciSequence(n) { 
+    let fibonacci = [0, 1]; 
+    
+    for (let i = 2; i < n; i++) { 
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]; 
+    
+    } return fibonacci.slice(0, n); 
+} 
+    
+console.log(fibonacciSequence(10));
