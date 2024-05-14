551c97cd65c9eef13843303bdeae98d7c90acc1e
sofiaoishi <167095663+sofiaoishi@users.noreply.github.com>
2024-05-14 08:44:26 -0300

    Add files via upload

---
+++ exn2.js
@@ -0,0 +1,8 @@
+//FOR
+//2. Imprimir números pares de 1 a 10.
+
+for (let contador = 1; contador <= 10; contador ++) {
+    if (contador % 2 === 0) {
+        console.log(contador);
+    }
+}
---
+++ exn3.js
@@ -0,0 +1,8 @@
+//FOR
+//3. Faça um programa que imprima os números ímpares de 0 a 50;
+
+for (let contador = 1; contador <= 50; contador ++) {
+    if (contador % 2 != 0) {
+        console.log(contador);
+    }
+}
